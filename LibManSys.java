1379742b34024e1ee0510e3e30653e4ece0916f5
Denver G. Cadungog <78331445+denbeo05@users.noreply.github.com>
2021-07-02 22:16:59 +0800

    Add files via upload

---
+++ LibManSys.java
@@ -0,0 +1,12 @@
+public class LibManSys {
+
+    public static void main(String[] args) {
+
+        System.out
+                .println("\n\n" + ConsoleColors.GREEN_BOLD + "LIBRARY MANAGEMENT SYSTEM" + ConsoleColors.RESET + "\n");
+        Commands.list();
+
+        Commands.init();
+
+    }
+}
